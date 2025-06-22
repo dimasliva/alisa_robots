0e176e46f5644b028032e6e79ee904499314412f
Дмитрий Ермилов <janzerikYT@yandex.ru>
2025-06-22 14:48:56 +0500

    feat(books): add pages

--- scripts/book.js
+++ scripts/book.js
@@ -3,8 +3,19 @@ document.addEventListener("DOMContentLoaded", () => {
   const img = page.querySelector("img");
 
   const srcArray = [
-    "./svgs/Снимок экрана 2025-05-16 142216 1.svg",
-    "./svgs/Frame 119.svg",
+    "./svgs/books/1.svg",
+    "./svgs/books/2.svg",
+    "./svgs/books/3.svg",
+    "./svgs/books/4.svg",
+    "./svgs/books/5.svg",
+    "./svgs/books/6.svg",
+    "./svgs/books/7.svg",
+    "./svgs/books/8.svg",
+    "./svgs/books/9.svg",
+    "./svgs/books/10.svg",
+    "./svgs/books/11.svg",
+    "./svgs/books/12.svg",
+    "./svgs/books/13.svg",
   ];
 
   // Текущий индекс в массиве
